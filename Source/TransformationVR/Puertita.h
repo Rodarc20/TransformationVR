1acc4c911454cf46650705af70211eae2541f9f0
Rodrigo Gutierrez <rodrigo.gutierrez.k@gmail.com>
2018-02-17 21:44:07 -0500

    Agregados modelos, y la clase puerta.

---
+++ Source/TransformationVR/Puertita.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Bloque.h"
+#include "Puertita.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRANSFORMATIONVR_API APuertita : public ABloque
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
