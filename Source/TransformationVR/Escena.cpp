30e2e0eaca73ea8478b6f36e2547ff286e1a5e51
Rodrigo Gutierrez <rodrigo.gutierrez.k@gmail.com>
2018-03-02 23:17:24 -0500

    Funconalidad de seleccion de ejes de escena, prueba para material titilante

--- Source/TransformationVR/Escena.cpp
+++ Source/TransformationVR/Escena.cpp
@@ -8,6 +8,7 @@
 #include "Engine/Engine.h"
 #include "TransformacionWidget.h"
 #include "Engine/StaticMesh.h"
+#include "Casa.h"
 
 
 // Sets default values
@@ -74,6 +75,13 @@ AEscena::AEscena() {
     FlechaY->SetCollisionProfileName(FName(TEXT("Transformacion")));
     FlechaZ->SetCollisionProfileName(FName(TEXT("Transformacion")));
 
+    FlechaX->OnComponentBeginOverlap.AddDynamic(this, &AEscena::OnBeginOverlapEjeX);
+    FlechaX->OnComponentEndOverlap.AddDynamic(this, &AEscena::OnEndOverlapEjeX);
+    FlechaY->OnComponentBeginOverlap.AddDynamic(this, &AEscena::OnBeginOverlapEjeY);
+    FlechaY->OnComponentEndOverlap.AddDynamic(this, &AEscena::OnEndOverlapEjeY);
+    FlechaZ->OnComponentBeginOverlap.AddDynamic(this, &AEscena::OnBeginOverlapEjeZ);
+    FlechaZ->OnComponentEndOverlap.AddDynamic(this, &AEscena::OnEndOverlapEjeZ);
+
     //TWidget->OcultarWidget();
 
 }
@@ -82,12 +90,71 @@ AEscena::AEscena() {
 void AEscena::BeginPlay() {
 	Super::BeginPlay();
 	
+    EnableInput(GetWorld()->GetFirstPlayerController());
+    bBlockInput = 0;
+    //AutoReceiveInput = EAutoReceiveInput::Player0;
+    
+     //UInputComponent* myInputComp = this->InputComponent;  //InputComponent variable is from AActor.h
+     //check that it is valid before calling
+     //if (myInputComp) {
+    if(this->InputComponent){
+         //SetupPlayerInputComponent(myInputComp);
+         SetupPlayerInputComponent(this->InputComponent);
+     }
+
+    TArray<AActor *> CasasEncontradas;
+    UGameplayStatics::GetAllActorsOfClass(GetWorld(), ACasa::StaticClass(), CasasEncontradas);
+    //UE_LOG(LogClass, Log, TEXT("Numero de Partes Encontradas: %d"), PartesEncontradas.Num());
+    if (CasasEncontradas.Num()) {
+        ACasa * const CasaEncontrada = Cast<ACasa>(CasasEncontradas[0]);
+        if (CasaEncontrada) {
+            UE_LOG(LogClass, Log, TEXT("Casa para escena encontrada"));
+            Casa = CasaEncontrada;
+        }
+    }
 }
 
 // Called every frame
 void AEscena::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
 
+
+    //verificar si alguno de los ejes esta siendo tocado por algun control
+    //debo buscar coincidencia, es decir debo asegurarme de estar si algun eje se toca, si es con el control derecho debe ser seleccionado solo cuando se presione grabRight
+}
+
+void AEscena::SetupPlayerInputComponent(UInputComponent * PlayerInputComponent) {
+    //PlayerInputComponent->bBlockInput = 0;
+    PlayerInputComponent->BindAction("GrabRight", IE_Pressed, this, &AEscena::GrabRightPressed).bConsumeInput = 0;
+    PlayerInputComponent->BindAction("GrabRight", IE_Released, this, &AEscena::GrabRightReleased).bConsumeInput = 0;
+
+    PlayerInputComponent->BindAction("GrabLeft", IE_Pressed, this, &AEscena::GrabLeftPressed).bConsumeInput = 0;
+    PlayerInputComponent->BindAction("GrabLeft", IE_Released, this, &AEscena::GrabLeftReleased).bConsumeInput = 0;
+}
+//debo tenerlos efectos de eje presseda, eje birando, eje seleccionado, y cuando se selecciona al soltar el grab, debo mndar la señal de seleccionar eje en la casa
+
+void AEscena::GrabRightPressed() {
+}
+
+void AEscena::GrabRightTick() {
+}
+
+void AEscena::GrabRightReleased() {
+    if (OverlapedRightEjes.Num() && Casa) {
+        Casa->SetTransformacionEje(OverlapedRightEjes[0]);
+    }
+}
+
+void AEscena::GrabLeftPressed() {
+}
+
+void AEscena::GrabLeftTick() {
+}
+
+void AEscena::GrabLeftReleased() {
+    if (OverlapedLeftEjes.Num() && Casa) {
+        Casa->SetTransformacionEje(OverlapedLeftEjes[0]);
+    }
 }
 
 void AEscena::MostrarWidget() {
@@ -104,3 +171,150 @@ void AEscena::OcultarWidget() {
 	FlechaZ->SetVisibility(false);
 }
 
+void AEscena::OnBeginOverlapEjeX(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult) {
+    if (Casa && Casa->GetCasaTask() == EVRCasaTask::EPlayTask) {
+        if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr) && (OtherActor != GetOwner())) { //no es necesario el ultimo, solo para este caso particular en el que no quiero que el propio conejo active esta funconalidad
+            AVRPawn * const VRPawn = Cast<AVRPawn>(OtherActor);
+            if (VRPawn && !VRPawn->IsPendingKill()) {
+                UCapsuleComponent * const ColisionController = Cast<UCapsuleComponent>(OtherComp);//para la casa no necesito verificar que haya tocado su staticmesh
+                if (ColisionController) {
+                    if (ColisionController->GetName() == "ColisionControllerRight") {
+                        OverlapedRightEjes.AddUnique(ETransformacionEje::EEjeX);
+                        if (GEngine)//no hacer esta verificación provocaba error al iniciar el editor
+                            GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, TEXT("Overlaped Eje X con ControllerRight"));
+                    }
+                    else if (ColisionController->GetName() == "ColisionControllerLeft") {
+                        OverlapedLeftEjes.AddUnique(ETransformacionEje::EEjeX);
+                        if (GEngine)//no hacer esta verificación provocaba error al iniciar el editor
+                            GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, TEXT("Overlaped Eje Y con ControllerLeft"));
+                    }
+                }
+            }
+        }
+    }
+}
+
+void AEscena::OnBeginOverlapEjeY(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult) {
+    if (Casa && Casa->GetCasaTask() == EVRCasaTask::EPlayTask) {
+        if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr) && (OtherActor != GetOwner())) { //no es necesario el ultimo, solo para este caso particular en el que no quiero que el propio conejo active esta funconalidad
+            AVRPawn * const VRPawn = Cast<AVRPawn>(OtherActor);
+            if (VRPawn && !VRPawn->IsPendingKill()) {
+                UCapsuleComponent * const ColisionController = Cast<UCapsuleComponent>(OtherComp);//para la casa no necesito verificar que haya tocado su staticmesh
+                if (ColisionController) {
+                    if (ColisionController->GetName() == "ColisionControllerRight") {
+                        OverlapedRightEjes.AddUnique(ETransformacionEje::EEjeY);
+                        if (GEngine)//no hacer esta verificación provocaba error al iniciar el editor
+                            GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, TEXT("Overlaped Eje Y con ControllerRight"));
+                    }
+                    else if (ColisionController->GetName() == "ColisionControllerLeft") {
+                        OverlapedLeftEjes.AddUnique(ETransformacionEje::EEjeY);
+                        if (GEngine)//no hacer esta verificación provocaba error al iniciar el editor
+                            GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, TEXT("Overlaped Eje Y con ControllerLeft"));
+                    }
+                }
+            }
+        }
+    }
+}
+
+void AEscena::OnBeginOverlapEjeZ(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult) {
+    if (Casa && Casa->GetCasaTask() == EVRCasaTask::EPlayTask) {
+        if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr) && (OtherActor != GetOwner())) { //no es necesario el ultimo, solo para este caso particular en el que no quiero que el propio conejo active esta funconalidad
+            AVRPawn * const VRPawn = Cast<AVRPawn>(OtherActor);
+            if (VRPawn && !VRPawn->IsPendingKill()) {
+                UCapsuleComponent * const ColisionController = Cast<UCapsuleComponent>(OtherComp);//para la casa no necesito verificar que haya tocado su staticmesh
+                if (ColisionController) {
+                    if (ColisionController->GetName() == "ColisionControllerRight") {
+                        OverlapedRightEjes.AddUnique(ETransformacionEje::EEjeZ);
+                        if (GEngine)//no hacer esta verificación provocaba error al iniciar el editor
+                            GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, TEXT("Overlaped Eje Z con ControllerRight"));
+                    }
+                    else if (ColisionController->GetName() == "ColisionControllerLeft") {
+                        OverlapedLeftEjes.AddUnique(ETransformacionEje::EEjeZ);
+                        if (GEngine)//no hacer esta verificación provocaba error al iniciar el editor
+                            GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, TEXT("Overlaped Eje Z con ControllerLeft"));
+                    }
+                }
+            }
+        }
+    }
+}
+
+void AEscena::OnEndOverlapEjeX(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex) {
+    if (Casa && Casa->GetCasaTask() == EVRCasaTask::EPlayTask) {
+        if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr) && (OtherActor != GetOwner())) { //no es necesario el ultimo, solo para este caso particular en el que no quiero que el propio conejo active esta funconalidad
+            AVRPawn * const VRPawn = Cast<AVRPawn>(OtherActor);
+            if (VRPawn && !VRPawn->IsPendingKill()) {
+                UCapsuleComponent * const ColisionController = Cast<UCapsuleComponent>(OtherComp);//para la casa no necesito verificar que haya tocado su staticmesh
+                if (ColisionController) {
+                    if (ColisionController->GetName() == "ColisionControllerRight") {
+                        OverlapedRightEjes.Remove(ETransformacionEje::EEjeX);
+                        if (GEngine)//no hacer esta verificación provocaba error al iniciar el editor
+                            GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, TEXT("End Overlaped Eje X con ControllerRight"));
+                    }
+                    else if (ColisionController->GetName() == "ColisionControllerLeft") {
+                        OverlapedLeftEjes.Remove(ETransformacionEje::EEjeX);
+                        if (GEngine)//no hacer esta verificación provocaba error al iniciar el editor
+                            GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, TEXT("End Overlaped Eje X con ControllerLeft"));
+                    }
+                }
+            }
+        }
+    }
+}
+
+void AEscena::OnEndOverlapEjeY(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex) {
+    if (Casa && Casa->GetCasaTask() == EVRCasaTask::EPlayTask) {
+        if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr) && (OtherActor != GetOwner())) { //no es necesario el ultimo, solo para este caso particular en el que no quiero que el propio conejo active esta funconalidad
+            AVRPawn * const VRPawn = Cast<AVRPawn>(OtherActor);
+            if (VRPawn && !VRPawn->IsPendingKill()) {
+                UCapsuleComponent * const ColisionController = Cast<UCapsuleComponent>(OtherComp);//para la casa no necesito verificar que haya tocado su staticmesh
+                if (ColisionController) {
+                    if (ColisionController->GetName() == "ColisionControllerRight") {
+                        OverlapedRightEjes.Remove(ETransformacionEje::EEjeY);
+                        if (GEngine)//no hacer esta verificación provocaba error al iniciar el editor
+                            GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, TEXT("End Overlaped Eje Y con ControllerRight"));
+                    }
+                    else if (ColisionController->GetName() == "ColisionControllerLeft") {
+                        OverlapedLeftEjes.Remove(ETransformacionEje::EEjeY);
+                        if (GEngine)//no hacer esta verificación provocaba error al iniciar el editor
+                            GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, TEXT("End Overlaped Eje Y con ControllerLeft"));
+                    }
+                }
+            }
+        }
+    }
+}
+
+void AEscena::OnEndOverlapEjeZ(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex) {
+    if (Casa && Casa->GetCasaTask() == EVRCasaTask::EPlayTask) {
+        if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr) && (OtherActor != GetOwner())) { //no es necesario el ultimo, solo para este caso particular en el que no quiero que el propio conejo active esta funconalidad
+            AVRPawn * const VRPawn = Cast<AVRPawn>(OtherActor);
+            if (VRPawn && !VRPawn->IsPendingKill()) {
+                UCapsuleComponent * const ColisionController = Cast<UCapsuleComponent>(OtherComp);//para la casa no necesito verificar que haya tocado su staticmesh
+                if (ColisionController) {
+                    if (ColisionController->GetName() == "ColisionControllerRight") {
+                        OverlapedRightEjes.Remove(ETransformacionEje::EEjeZ);
+                        if (GEngine)//no hacer esta verificación provocaba error al iniciar el editor
+                            GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, TEXT("End Overlaped Eje Z con ControllerRight"));
+                    }
+                    else if (ColisionController->GetName() == "ColisionControllerLeft") {
+                        OverlapedLeftEjes.Remove(ETransformacionEje::EEjeZ);
+                        if (GEngine)//no hacer esta verificación provocaba error al iniciar el editor
+                            GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, TEXT("End Overlaped Eje Z con ControllerLeft"));
+                    }
+                }
+            }
+        }
+    }
+}
+
+//voy a tener dos array, uno para el control derecho y otro para el control izquierdo
+//con el cual estas funciones lo unico que hace ens ingresar su respectivo eje a esos arrays cuando entren en contacto con el control correspondiente a cada array
+//y lo saco el eje, cuando el control deje de estar en contacto
+//entonces cuando presione el trigger de cada no, debo detectar si en el array ahay algo, pouedo ahcer la jugada de coger el que esta mas cerca, pero por ahora seleccionare solo el primer elemento
+//estos array no contendran el eje, lo que contendra sera el enum, para asi manejar todo solo pasando ese parametro, igual que los ejectos
+
+
+//necestio estados para los efectos visuales de los ejes, los cuales funcionan, en distintas partes de esta clase, solo la animacion del titilando estara controlada en el tick
+//aun que tambien los materiales puede ejecutar esa animacion, depende de como lo quieras manejar, lo mejor seria que sea independiente
--- Source/TransformationVR/Escena.h
+++ Source/TransformationVR/Escena.h
@@ -7,6 +7,7 @@
 #include "GameFramework/Actor.h"
 #include "Materials/Material.h"
 #include "TransformacionWidget.h"
+#include "Casa.h"
 #include "Escena.generated.h"
 
 UCLASS()
@@ -26,6 +27,7 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent);
 
     //usar felchas mas grandes
     /*UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Transformation")
@@ -43,10 +45,55 @@ public:
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Transformation")
     UStaticMeshComponent * Origen;
 
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Transformation")
+    ACasa * Casa;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Transformation")
+    TArray<ETransformacionEje> OverlapedLeftEjes;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Transformation")
+    TArray<ETransformacionEje> OverlapedRightEjes;
+
+
+    UFUNCTION(BlueprintCallable, Category = "Transformation")
+    void GrabRightPressed();
+
+    UFUNCTION(BlueprintCallable, Category = "Transformation")
+    void GrabRightTick();
+
+    UFUNCTION(BlueprintCallable, Category = "Transformation")
+    void GrabRightReleased();
+
+    UFUNCTION(BlueprintCallable, Category = "Transformation")
+    void GrabLeftPressed();
+
+    UFUNCTION(BlueprintCallable, Category = "Transformation")
+    void GrabLeftTick();
+
+    UFUNCTION(BlueprintCallable, Category = "Transformation")
+    void GrabLeftReleased();
+
     void MostrarWidget();
 
     void OcultarWidget();
 
+    UFUNCTION()
+    void OnBeginOverlapEjeX(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+
+    UFUNCTION()
+    void OnBeginOverlapEjeY(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+
+    UFUNCTION()
+    void OnBeginOverlapEjeZ(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+
+    UFUNCTION()
+	void OnEndOverlapEjeX(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex);
+
+    UFUNCTION()
+	void OnEndOverlapEjeY(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex);
+
+    UFUNCTION()
+	void OnEndOverlapEjeZ(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex);
     //esta clase sera interactiva, seran los botones, algo asi como el panel, busar una colision apropiadaa para ellos para que puedan ser seleccionados
     //que sea la misma de los ejes, para que se puedan seleccionar, lo mismo en lo que es trasnformacionwidget, ya que esos pequeños tambien se seleccionaran
     //en la tarea rotacion
